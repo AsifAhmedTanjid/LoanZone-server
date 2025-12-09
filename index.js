d170a3ab2f82e54e9039b64f2d5fcac2e815aeec
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2025-12-09 13:23:36 +0600

    init: setup mongodb and connected to the db using env

---
+++ index.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.clghzkh.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("server is running bro");
+});
+
+async function run() {
+  try {
+    const db = client.db("loanzone-db");
+
+
+
+    // APIs 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+
