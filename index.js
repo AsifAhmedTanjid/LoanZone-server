0c6882a1fb1ceff35a2a8ac5697dd22ef085ac84
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2025-12-10 12:30:14 +0600

    feat: added user post api to insert user data to db

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+var admin = require("firebase-admin");
+const serviceAccount = require("./loan-zone-firebase-adminsdk.json");
 const app = express();
 const port = 3000;
 
@@ -9,6 +11,9 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.clghzkh.mongodb.net/?appName=Cluster0`;
 
@@ -24,31 +29,69 @@ app.get("/", (req, res) => {
   res.send("server is running bro");
 });
 
+
+const verifyToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.status(401).send({
+      message: "unauthorized access. Token not found!",
+    });
+  }
+
+  const token = authorization.split(" ")[1];
+  try {
+    const decodedUser = await admin.auth().verifyIdToken(token);
+    req.user = decodedUser;
+
+    next();
+  } catch (error) {
+    res.status(401).send({
+      message: "unauthorized access.",
+    });
+  }
+};
+
 async function run() {
   try {
     const db = client.db("loanzone-db");
+    const userCollection = db.collection("users");
+
+    // APIs
+
+    // user api  
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+        if (!user.role) {
+         user.role = "borrower";
+      }
+
+      user.createdAt = new Date();
+      const email = user.email;
+      const userExists = await userCollection.findOne({ email });
+
+      if (userExists) {
+        return res.send({ message: "user exists" });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
 
 
-    // APIs 
+
+
+
+
+
+
+
+
+
     
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
@@ -63,5 +106,3 @@ run().catch(console.dir);
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
-
-
